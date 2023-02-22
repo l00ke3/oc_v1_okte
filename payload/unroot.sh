f027739cf33ac4d75f68b7b3ba1bce4468ce4e56
cata0nana <101589752+cata0nana@users.noreply.github.com>
2023-02-22 09:17:01 +0100

    Update unroot.sh

--- payload/unroot.sh
+++ payload/unroot.sh
@@ -57,4 +57,5 @@ sed -i 's/console/anybody/g' /etc/X11/Xwrapper.config
 dpkg-reconfigure xserver-xorg-legacy allowed_users=anybody
 
 cat /etc/X11/Xwrapper.config
+echo "export PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '" >> /home/pythony/.bashrc
 
