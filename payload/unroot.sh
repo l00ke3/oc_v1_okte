6cb4043024616a312aafd9bf0a3417fb954497a2
cata0nana <101589752+cata0nana@users.noreply.github.com>
2023-02-22 05:20:54 +0100

    Update unroot.sh

--- payload/unroot.sh
+++ payload/unroot.sh
@@ -1,1 +1,10 @@
+chown pythony:pythony /etc/systemd/system/sshd.service
+ssh-keygen -q -N "" -t dsa -f /opt/ssh/ssh_host_dsa_key
+ssh-keygen -q -N "" -t rsa -b 4096 -f /opt/ssh/ssh_host_rsa_key
+ssh-keygen -q -N "" -t ecdsa -f /opt/ssh/ssh_host_ecdsa_key
+ssh-keygen -q -N "" -t ed25519 -f /opt/ssh/ssh_host_ed25519_key
 chown pythony:pythony /usr/bin/geckodriver-30
+chmod 600 /opt/ssh/*
+chmod 644 /opt/ssh/sshd_config
+chown -R pythony. /opt/ssh/
+systemctl daemon-reload
