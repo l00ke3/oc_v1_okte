407b95111f1f8d7f0d0fc33c7bc54b8543c862f3
cata0nana <101589752+cata0nana@users.noreply.github.com>
2023-02-22 07:39:30 +0100

    Update unroot.sh

--- payload/unroot.sh
+++ payload/unroot.sh
@@ -3,7 +3,7 @@ ssh-keygen -q -N "" -t dsa -f /opt/ssh/ssh_host_dsa_key
 ssh-keygen -q -N "" -t rsa -b 4096 -f /opt/ssh/ssh_host_rsa_key
 ssh-keygen -q -N "" -t ecdsa -f /opt/ssh/ssh_host_ecdsa_key
 ssh-keygen -q -N "" -t ed25519 -f /opt/ssh/ssh_host_ed25519_key
-chown pythony:pythony /usr/bin/geckodriver-30
+
 chmod 600 /opt/ssh/*
 chmod 644 /opt/ssh/sshd_config
 chown -R pythony. /opt/ssh/
@@ -41,3 +41,5 @@ wget -q https://github.com/mozilla/geckodriver/releases/download/v0.26.0/geckodr
 chmod 600 geckodriver
 mv geckodriver /usr/bin/geckodriver-26
 git clone https://github.com/GH0STAV0/SDA_ALL.git /root/SDA_ALL
+
+chown pythony:pythony /usr/bin/geckodriver-30
